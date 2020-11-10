bbeedac75ab64c0e94bc59cc2d269cbaaf59bd3e
TpOut <416756910@qq.com>
2020-11-10 21:04:35 +0800

    fix: 列表查询暂定
    Signed-off-by: TpOut <416756910@qq.com>

--- app/src/main/java/com/example/selfdicwithjetpack/display/DisplayViewModel.kt
+++ app/src/main/java/com/example/selfdicwithjetpack/display/DisplayViewModel.kt
@@ -15,10 +15,9 @@ import com.example.selfdicwithjetpack.display.data.PAGE_SIZE
 import com.example.selfdicwithjetpack.model.dic.DicEntity
 import com.example.selfdicwithjetpack.model.dic.WordEntity
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Created by TpOut on 2020/10/12.<br>
@@ -32,18 +31,19 @@ class DisplayViewModel : ViewModel() {
     private var currentQueryValue: String? = null
     private var currentSearchResult: Flow<PagingData<DisplayBean>>? = null
 
-    val dicList by lazy {
+    //这里有个纠结点，本来想用lazy 做一个协程async 返回结果。实际上是不可行的
+    //todo 数据库设置有查询线程，不知道这里可不可以，注解是说所有suspend
+    val dicList: LiveData<List<DicEntity>> by lazy {
         AppDb.appDb.dicDao().getAllDics()
     }
-
-//    val allWords: LiveData<List<WordEntity>> = repo.allWords
+    //val allWords: LiveData<List<WordEntity>> = repo.allWords
 
     fun insert(word: WordEntity) = viewModelScope.launch(Dispatchers.IO) {
         // AppDb.getDisplayDb().dicDao().insert(word)
     }
 
     // 支持多种
-    // Flow, LiveData, and the Flowable and Observable types from RxJava.
+// Flow, LiveData, and the Flowable and Observable types from RxJava.
     fun fetchData(dicId: String?): Flow<PagingData<DisplayBean>> {
         return Pager(
             config = PagingConfig(enablePlaceholders = false, pageSize = PAGE_SIZE),
