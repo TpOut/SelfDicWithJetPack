9c8dc3ec9ec362711d9ec95df7ffbeecb00cec57
TpOut <416756910@qq.com>
2021-01-22 19:59:02 +0800

    约束服务器处理结果
    Signed-off-by: TpOut <416756910@qq.com>

--- app/src/main/java/com/example/selfdicwithjetpack/api/yourena/YourenaResultBean.kt
+++ app/src/main/java/com/example/selfdicwithjetpack/api/yourena/YourenaResultBean.kt
@@ -5,6 +5,8 @@ import com.example.selfdicwithjetpack.model.dic.WordEntity
 /**
  * Created by TpOut on 2020/10/12.<br>
  * Email address: 416756910@qq.com<br>
+ *
+ * resultStatus : success / fail
  */
-data class QueryWordResultBean(val result: List<WordEntity>, val page: Int)
-data class AddWordResultBean(val result: String)
+data class QueryWordResultBean(val resultStatus: String, val result: List<WordEntity>, val page: Int)
+data class AddWordResultBean(val resultStatus: String)
