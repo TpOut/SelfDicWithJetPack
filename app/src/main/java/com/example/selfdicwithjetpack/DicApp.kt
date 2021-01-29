bed8c7ed1eb843a09950cd2aeb3bed366b22b230
TpOut <416756910@qq.com>
2021-01-29 18:30:47 +0800

    strictmode
    Signed-off-by: TpOut <416756910@qq.com>

--- app/src/main/java/com/example/selfdicwithjetpack/DicApp.kt
+++ app/src/main/java/com/example/selfdicwithjetpack/DicApp.kt
@@ -1,6 +1,7 @@
 package com.example.selfdicwithjetpack
 
 import android.app.Application
+import android.os.StrictMode
 import com.blankj.utilcode.util.LogUtils
 import com.example.selfdicwithjetpack.component.data.Sp
 import com.example.selfdicwithjetpack.component.debug.log.LogUtil
@@ -9,15 +10,35 @@ import com.example.selfdicwithjetpack.component.debug.log.LogUtil
  * Created by TpOut on 2020/10/20.<br>
  * Email address: 416756910@qq.com<br>
  */
-class DicApp  : Application(){
+class DicApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        LogUtil
+        setStrictMode()
         Sp.init(this)
         Thread.setDefaultUncaughtExceptionHandler { t, e ->
             LogUtils.d("AppUncaught", e.printStackTrace())
+        }
+    }
+
+    private fun setStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build()
+            );
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            );
         }
     }
 
