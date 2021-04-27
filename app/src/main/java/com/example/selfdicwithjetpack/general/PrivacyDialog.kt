a6dc00e15c28f0987c1f4288548cba53d5834f1e
TpOut <416756910@qq.com>
2021-04-27 21:53:11 +0800

    弹窗
    Signed-off-by: TpOut <416756910@qq.com>

---
+++ app/src/main/java/com/example/selfdicwithjetpack/general/PrivacyDialog.kt
@@ -0,0 +1,56 @@
+package com.example.selfdicwithjetpack.general
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+
+/**
+ * Created by TpOut on 2021/4/27.<br>
+ * Email address: 416756910@qq.com<br>
+ */
+class PrivacyDialog : DialogFragment() {
+
+    //直接使用弹窗，互斥onCreateView
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setTitle("隐私弹窗")
+            .setMessage("这是隐私弹窗内容哦")
+            .setPositiveButton("好的") { _, _ -> }
+            .setNegativeButton("不好") { _, _ ->
+
+            }
+            .create()
+
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+
+    }
+
+    // 自定义view，互斥onCreateDialog
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    fun show(manager: FragmentManager) {
+        super.show(manager, tag)
+    }
+
+    companion object {
+        const val TAG = "PrivacyDialog"
+    }
+}
