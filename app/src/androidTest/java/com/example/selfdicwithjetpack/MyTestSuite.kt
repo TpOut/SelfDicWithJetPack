e09704eb9de93f99f92ea708d9e791ae5f033c32
TpOut <416756910@qq.com>
2021-04-28 19:58:42 +0800

    fragment 测试增加
    Signed-off-by: TpOut <416756910@qq.com>

--- app/src/androidTest/java/com/example/selfdicwithjetpack/MyTestSuite.kt
+++ app/src/androidTest/java/com/example/selfdicwithjetpack/MyTestSuite.kt
@@ -1,6 +1,10 @@
 package com.example.selfdicwithjetpack
 
 import android.util.Log
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.fragment.app.testing.launchFragment
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
@@ -8,6 +12,8 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.selfdicwithjetpack.general.PrivacyDialog
+import com.example.selfdicwithjetpack.login.LoginFrag
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -42,10 +48,16 @@ class MyTestSuite {
 
     @Test
     fun testFragment(){
+
 //        val dessertRepository = mock(DessertsRepository::class.java)
-//        launchFragment<DessertsFragment>(factory = MyFragmentFactory(dessertRepository)).onFragment {
-//            // Test Fragment logic
-//        }
+//        launchFragment<DessertsFragment>(factory = MyFragmentFactory(dessertRepository)) // 直接运行，不依赖activity（创建一个空的
+
+//        launchFragment<PrivacyDialog>() // dialog 也不依赖
+
+        val fragmentArgs = bundleOf("sddsf" to 0)
+        val scenario = launchFragmentInContainer<LoginFrag>(fragmentArgs) // 依赖一个activity
+//        scenario.moveToState()
+        // 后续和activity 的类似
     }
 }
     
