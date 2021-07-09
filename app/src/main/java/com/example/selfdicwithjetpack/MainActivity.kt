552cfd4f0b4b3fab4ff6ed33ec05cfa1d25f67e4
TpOut <416756910@qq.com>
2021-07-09 17:43:59 +0800

    设置页，使用panellayout，temp
    Signed-off-by: TpOut <416756910@qq.com>

--- app/src/main/java/com/example/selfdicwithjetpack/MainActivity.kt
+++ app/src/main/java/com/example/selfdicwithjetpack/MainActivity.kt
@@ -17,7 +17,6 @@ import com.blankj.utilcode.util.ToastUtils
 import com.example.selfdicwithjetpack.component.data.MmkvStorage
 import com.example.selfdicwithjetpack.component.schedule.singleWork
 import com.example.selfdicwithjetpack.display.DisplayViewModel
-import com.example.selfdicwithjetpack.general.PrivacyDialog
 import com.example.selfdicwithjetpack.random.RandomWidgetProvider
 
 const val MAIN_ACTIVITY_TAG = "MainActivity"
---
+++ app/src/main/java/com/example/selfdicwithjetpack/setting/SettingFrag.kt
@@ -0,0 +1,17 @@
+package com.example.selfdicwithjetpack.setting
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by TpOut on 2021/7/8.<br>
+ * Email address: 416756910@qq.com<br>
+ */
+class SettingFrag: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
